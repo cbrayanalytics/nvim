103d1166197ed8f8370b2a01349a38e9969959e5
Casey Bray <cbrayanalytics@gmail.com>
2025-05-28 00:16:02 -0600

    Add customizations to markdown

--- lua/casey/plugins/rendermarkdown.lua
+++ lua/casey/plugins/rendermarkdown.lua
@@ -5,5 +5,54 @@ return {
 	dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
 	---@module 'render-markdown'
 	---@type render.md.UserConfig
-	opts = {},
+	config = function()
+		require("render-markdown").setup({
+
+			heading = {
+				border = true,
+				border_virtual = true,
+				icons = { "󰉫 ", "󰉬 ", "󰉭 ", "󰉮 ", "󰉯 ", "󰉰 " },
+				sign = true,
+				signs = { " " },
+			},
+			dash = {
+				icon = "",
+			},
+			bullet = {
+				icons = { "󰳳", "󰳳" },
+				ordered_icons = function(ctx)
+					local value = vim.trim(ctx.value)
+					local index = tonumber(value:sub(1, #value - 1))
+					return ("%d."):format(index > 1 and index or ctx.index)
+				end,
+				left_pad = 0.5,
+				right_pad = 1,
+				highlight = "RenderMarkdownBullet",
+				scope_highlight = {},
+			},
+			checkbox = {
+				enabled = true,
+				render_modes = false,
+				right_pad = 1,
+				unchecked = {
+					icon = "✘",
+					highlight = "RenderMarkdownUnchecked",
+					scope_highlight = nil,
+				},
+				checked = {
+					icon = "✔",
+					highlight = "RenderMarkdownChecked",
+					scope_highlight = "@markup.strikethrough",
+				},
+				custom = {
+					todo = {
+						raw = "[-]",
+						rendered = "󰥕",
+						highlight = "RenderMarkdownTodo",
+						scope_highlight = nil,
+					},
+				},
+			},
+		})
+	end,
 }
